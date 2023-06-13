6e9c5119b1e0b04dcc381145fea165186d663d3c
mkreyman <mark@kreyman.com>
2023-06-13 09:20:10 -0600

    Working notes index and delete

--- lib/note_app/notes.ex
+++ lib/note_app/notes.ex
@@ -8,6 +8,19 @@ defmodule NoteApp.Notes do
     registry NoteApp.Notes.Registry
   end
 
+  @doc """
+  Returns the list of notes.
+
+  ## Examples
+
+      iex> list_notes()
+      [%Note{}, ...]
+
+  """
+  def list_notes() do
+    list_notes!()
+  end
+
   def list_notes!() do
     Note.read_all!
       |> Enum.map(fn note ->
@@ -15,17 +28,82 @@ defmodule NoteApp.Notes do
       end)
   end
 
+  @doc """
+  Gets a single note.
+
+  Raises `Ecto.NoResultsError` if the Note does not exist.
+
+  ## Examples
+
+      iex> get_note!(123)
+      %Note{}
+
+      iex> get_note!(456)
+      ** (Ecto.NoResultsError)
+
+  """
   def get_note!(id) do
     Note.get_by_id!(id)
   end
 
-  def delete_note!(note) do
-    Note.get_by_id!(note.id) |> Note.destroy!()
+  @doc """
+  Creates a note.
+
+  ## Examples
+
+      iex> create_note(%{field: value})
+      {:ok, %Note{}}
+
+      iex> create_note(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_note(attrs \\ %{}) do
+    Note.create(attrs)
   end
 
-  def change_note(note) do
-    note.id
-    |> NoteApp.Note.get_by_id!()
-    |> Ash.Changeset.for_update(:update)
+  @doc """
+  Updates a note.
+
+  ## Examples
+
+      iex> update_note(note, %{field: new_value})
+      {:ok, %Note{}}
+
+      iex> update_note(note, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_note(%Note{} = note, attrs) do
+    Note.update(note, attrs)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking note changes.
+
+  ## Examples
+
+      iex> change_note(note)
+      %Ecto.Changeset{data: %Note{}}
+
+  """
+  def change_note(%Note{} = note, attrs \\ %{}) do
+    Ash.Changeset.for_update(note, :update, attrs)
+  end
+
+  @doc """
+  Deletes a note.
+
+  ## Examples
+
+      iex> delete_note(note)
+      {:ok, %Note{}}
+
+      iex> delete_note(note)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_note(note) do
+    {:ok, Note.destroy(note)}
   end
 end
